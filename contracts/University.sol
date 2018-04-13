b6fe51ddcfd388de143ee8cfc17eee5f200bb47a
Marc Cohen <marc@Marcs-MacBook-Pro.local>
2018-04-13 12:01:32 -0500

    Created University contract.

--- contracts/University.sol
+++ contracts/University.sol
@@ -2,9 +2,40 @@ pragma solidity ^0.4.21;
 
 contract University 
 {
+    address public owner;
     
-    mapping(student => degree)
-    mapping(degree => university)
+    
+    mapping(address => bool) public isAdministrator;
 
+    // Mapping of student to degree
+    mapping(bytes32 => bytes32[]) public studentDegree;
+
+
+    function University() public
+    {
+        owner = msg.sender;
+        isAdministrator[owner] = true;
+        
+    }
+
+    modifier onlyOwner {
+        require(owner == msg.sender);
+        _;
+    }
+
+    modifier onlyAdministrator
+    {
+        require(isAdministrator[msg.sender] == true);
+        _;
+    }
+
+    function addDegree(bytes32 studentId, bytes32 degreeId) public onlyAdministrator
+    {
+        studentDegree[studentId].push(degreeId);
+    }
+
+    function addAdmin(address newAdmin) public onlyOwner {
+        isAdministrator[newAdmin] = true;
+    }
 
 }
--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,5 +1,7 @@
 var Migrations = artifacts.require("./Migrations.sol");
+var University = artifacts.require("./University.sol");
 
 module.exports = function(deployer) {
   deployer.deploy(Migrations);
+  deployer.deploy(University);
 };
